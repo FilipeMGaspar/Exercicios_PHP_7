ddc6d4012987c67c9ee0e12a72f1a6eaaabe1ad9
fipewmc <fipewmc2@gmail.com>
2022-07-12 12:58:51 +0100

    Sistema de ponto para cadastrar horário de entrada e saída com PHP - Celke

--- Celke/Sistema_de_ponto/JS/script.js
+++ Celke/Sistema_de_ponto/JS/script.js
@@ -1,7 +1,12 @@
 var apHorario = document.querySelector("#horario");
 
 function atualizarHorario(){
+    var data = new Date().toLocaleString("pt-pt",{
+        timeZone: "Europe/Lisbon"
+    })
 
+    var formatarData = data.replace(", ", " - ")
+    apHorario.innerHTML = formatarData
 }
 
 setInterval(atualizarHorario(), 1000); // Chama a função a cad segundo
