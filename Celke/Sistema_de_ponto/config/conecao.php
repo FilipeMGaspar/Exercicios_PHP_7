0954b4b2d6efbbdb197d2b513dd0ce33143af322
fipewmc <fipewmc2@gmail.com>
2022-07-13 14:42:34 +0100

    Sistema de ponto para cadastrar horário de entrada e saída com PHP - Celke

--- Celke/Sistema_de_ponto/config/conecao.php
+++ Celke/Sistema_de_ponto/config/conecao.php
@@ -1,17 +1,17 @@
 <?php
 //Inicio da coneção com o banco de dados utilizando PDO
 $host = "localhost";
-$port = 3306;
+//$port = 3306;
 $dbname = "celke";
 $user = "root";
 $pass = "";
 
 try {
     // Conexão com utilização de porta utp
-    $conn = new PDO("mysql:host=$host;port=port;dbname=$dbname", $user, $pass);
+     // $conn = new PDO("mysql:host=$host;port=port;dbname=$dbname", $user, $pass);
 
     // Conexão sem porta UTP
-     //  $conn = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
+    $conn = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
 
     echo "Ligação á base de dados efectuada com sucesso";
 } catch (PDOException $err){
