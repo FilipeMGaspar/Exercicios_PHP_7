68d9ae355806c09907c13ac2b91fbfab47e18cd3
fipewmc <fipewmc2@gmail.com>
2022-07-13 10:04:12 +0100

    Sistema de ponto para cadastrar horário de entrada e saída com PHP - Celke

--- Celke/Sistema_de_ponto/registar_ponto.php
+++ Celke/Sistema_de_ponto/registar_ponto.php
@@ -5,5 +5,9 @@
 
     // Gerar com PHP o horário atual
     $horario_atual = date("H:i:s");
-
     var_dump($horario_atual);
+
+    // Gerar data com PHP da mesma forma que deve ser guardado na base de dados Ano/Mês/Dia
+    $data_entrada = date("Y/m/d");
+
+    // Incluir a conexão á base de dados 
