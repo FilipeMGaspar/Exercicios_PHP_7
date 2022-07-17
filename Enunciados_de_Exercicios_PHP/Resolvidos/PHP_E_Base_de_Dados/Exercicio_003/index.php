dd40ce491ec91b0828152d6d584c2617588fa14d
fipewmc <fipewmc2@gmail.com>
2022-07-17 14:05:36 +0100

    PHP e Base de Dados - Exercício 003

--- Enunciados_de_Exercicios_PHP/Resolvidos/PHP_E_Base_de_Dados/Exercicio_003/index.php
+++ Enunciados_de_Exercicios_PHP/Resolvidos/PHP_E_Base_de_Dados/Exercicio_003/index.php
@@ -19,10 +19,13 @@ Crie uma tabela de nome fornecedores na base de dados praticaphp utilzando o mys
     $conn = new mysqli($host, $user, $pass, $bd);
 
     //Criação da tabela fornecedores
-
-    $$query = "CREATE TABLE fornecedores (
+    $query = "CREATE TABLE fornecedores (
         codigo INT AUTO_INCREMENT PRIMARY KEY,
         nome VARCHAR(50) NOT NULL,
         email VARCHAR(50) NOT NULL,
         descricao TEXT
     );";
+
+    $conn->query($query); // Execução da query
+
+    $conn->close(); // Fecha a ligação á base de dados
