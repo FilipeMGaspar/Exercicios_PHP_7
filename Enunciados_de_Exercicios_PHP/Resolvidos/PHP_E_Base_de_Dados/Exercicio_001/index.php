cd9df6f38f2304d15cd6cca56c93ef3c9bce4727
fipewmc <fipewmc2@gmail.com>
2022-07-17 13:36:17 +0100

    PHP e Base de Dados - Exercício 001

--- Enunciados_de_Exercicios_PHP/Resolvidos/PHP_E_Base_de_Dados/Exercicio_001/index.php
+++ Enunciados_de_Exercicios_PHP/Resolvidos/PHP_E_Base_de_Dados/Exercicio_001/index.php
@@ -1,4 +1,6 @@
 <?php
     /* Crie uma ligação á base de dados praticaphp utilizando o mysqli, não deve ser utilizadas variáveis */ 
 
+     //$conn = new mysqli("host", "user", "password", "db");
+
     $conn = new mysqli("localhost", "root", "", "praticaphp");
