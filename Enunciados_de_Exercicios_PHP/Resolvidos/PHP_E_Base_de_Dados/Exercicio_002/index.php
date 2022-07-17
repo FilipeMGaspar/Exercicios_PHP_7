b6de5c87e4174f9ab91db2bd0cb5b8cd25e98e40
fipewmc <fipewmc2@gmail.com>
2022-07-17 13:41:49 +0100

    PHP e Base de Dados - Exercício 002

--- Enunciados_de_Exercicios_PHP/Resolvidos/PHP_E_Base_de_Dados/Exercicio_002/index.php
+++ Enunciados_de_Exercicios_PHP/Resolvidos/PHP_E_Base_de_Dados/Exercicio_002/index.php
@@ -1,2 +1,6 @@
 <?php
     /* Crie uma ligação á base de dados contas utilizando o mysqli, não deve ser utilizadas variáveis */
+
+    // $conn = new mysqli("hostname", "username", "password", "Base_de_dados")
+
+    $ligaBD = new mysqli("localhost");
