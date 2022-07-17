6e81d90515fe9088fb6a3fdd9d03a9008d61ac23
fipewmc <fipewmc2@gmail.com>
2022-07-17 14:10:01 +0100

    PHP e Base de Dados - Exercício 003 A

---
+++ Enunciados_de_Exercicios_PHP/Resolvidos/PHP_E_Base_de_Dados/Exercicio_003_A/index.php
@@ -0,0 +1,11 @@
+<?php
+ /*
+Crie uma tabela de nome cobradores na base de dados praticaphp utilzando o mysqli, a estrutura da tabela deverá ser a seguinte: 
+
+    id	Inteiro  auto incremento 
+    nome	Carater (50) não pode ser nulo
+    fornece	Caracter (50)
+    custo   decimal
+        Chave primária codigo
+
+*/
