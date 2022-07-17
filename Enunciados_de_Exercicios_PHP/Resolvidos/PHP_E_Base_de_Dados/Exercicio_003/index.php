99c4c2fee0498ebadbe18b58a2bf53e4d047f79a
fipewmc <fipewmc2@gmail.com>
2022-07-17 13:53:48 +0100

    PHP e Base de Dados - Exercício 003

---
+++ Enunciados_de_Exercicios_PHP/Resolvidos/PHP_E_Base_de_Dados/Exercicio_003/index.php
@@ -0,0 +1,11 @@
+<?php
+
+ /*
+Crie uma tabela de nome fornecedores na base de dados praticaphp utilzando o mysqli, a estrutura da tabela deverá ser a seguinte: 
+
+    codigo	Inteiro  auto incremento 
+    nome	Carater (50) não pode ser nulo
+    email	Caracter (50)
+        Chave primária codigo
+
+*/
