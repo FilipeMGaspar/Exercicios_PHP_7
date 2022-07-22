68a260e64977384e6a5e3e0d20c2c5a5fdf11740
fipewmc <fipewmc2@gmail.com>
2022-07-22 19:31:58 +0100

    Exercícios PHP 7 - Enunciado

---
+++ Enunciados_de_Exercicios_PHP/Resolvidos/PHP_E_Base_de_Dados/Exercicio_003_A/index.php
@@ -0,0 +1,26 @@
+<?php
+ /*
+Crie uma tabela de nome cobradores na base de dados contas utilzando o mysqli, a estrutura da tabela deverá ser a seguinte: 
+
+    id	Inteiro  auto incremento 
+    nome	Carater (50) não pode ser nulo
+    fornece	Caracter (50)
+    custo   decimal
+        Chave primária codigo
+
+*/
+
+//Ligação á base de dados com mysqli
+$host = "localhost";
+$user = "root";
+$pass = "";
+$db = "contas";
+
+$conn = new mysqli($host, $user, $pass, $db);
+
+$query = "CREATE TABLE contas (
+    idconta INT AUTO_INCREMENT PRIMARY KEY,
+    nome	VARCHAR(50), 
+    servico VARCHAR(50),
+    custo decimal
+);";
