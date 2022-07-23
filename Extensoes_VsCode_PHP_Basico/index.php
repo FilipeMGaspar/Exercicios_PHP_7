256e53290324f68ecfef11cfab5ac10ff982dbdc
fipewmc <fipewmc2@gmail.com>
2022-07-23 18:54:45 +0100

    Extensões para VsCode para PHP

---
+++ Extensoes_VsCode_PHP_Basico/index.php
@@ -0,0 +1,35 @@
+<?php
+    
+    echo "Olá, Mundo!";
+
+    /* Extenções para o VsCode para PHP */
+
+    /*
+    *   Utilizando o Xampp temos de adicionar as variaveis de ambiente
+
+    *   Twig   //Não gostei e não estou a utilizar
+  
+
+    *   DotENV -> Não estou a utilizar
+
+    *   EditorConfig for VS Code -> Não estou a utilizar
+
+    *   Code runner -> Em utilização
+
+    *   Path Intellisense -> Em utilização
+
+
+    * // Especificas para o PHP
+
+        *    PHP Intelephense -> Em utilização
+
+        *   PHP Getters & Setters -> Em utilização
+
+
+        *   PHP DocBlocker -> Para documentar  projetos  , não estou a utilizar
+
+        *   PHP Debug -> requer configuração, não estou a utilizar
+
+        *   PHP sniffer -> Para padronização psr12 , não estou a ttilizar
+
+    */
