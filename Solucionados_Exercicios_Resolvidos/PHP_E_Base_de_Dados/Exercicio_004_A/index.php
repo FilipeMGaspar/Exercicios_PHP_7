61ef9dcede8eae0592bb51918f63625687eef730
fipewmc <fipewmc2@gmail.com>
2022-07-24 19:55:10 +0100

    PHP e Base de Dados - Exercício 004 A

--- Solucionados_Exercicios_Resolvidos/PHP_E_Base_de_Dados/Exercicio_004_A/index.php
+++ Solucionados_Exercicios_Resolvidos/PHP_E_Base_de_Dados/Exercicio_004_A/index.php
@@ -1,22 +1,27 @@
 <?php
-    /*
-    Utilizando a base de dados ..: contas
-    Insira os seguintes dados na tabela ..: cobradores :.. utilizando o mysqli, sem utilizar o prepare e bind_param.
+  /* Enunciado ...:
 
-    codigo	          1	                         2	                          3
-    nome	        EDP	                        NOWO	                    SMASCB
-    servico     Eletricidade	            TV + NET + TLM	         Água e saneamento
-    custo	    45.56		                    40                          27.89
+      Utilizando a base de dados ..: contas
+      Insira os seguintes dados na tabela ..: cobradores :.. utilizando o mysqli, sem utilizar o prepare e bind_param.
 
-    */
+      codigo	          1	                         2	                          3
+      nome	        EDP	                        NOWO	                    SMASCB
+      servico     Eletricidade	            TV + NET + TLM	         Água e saneamento
+      custo	    45.56		                    40                          27.89
+*/
 
-//Ligação á base de dados
-$host = "localhost";
-$user  = "root";
-$pass = "";
-$db = "contas";
+  //Ligação á base de dados
+  $host = "localhost";
+  $user  = "root";
+  $pass = "";
+  $db = "contas";
 
-$conn = new mysqli($host, $user, $pass, $db);
+  $conn = new mysqli($host, $user, $pass, $db);
+
+  $query = "INSERT INTO cobradores (nome, servico, custo) VALUES ('EDP', 'Eletricidade', 45.56)";
+  $conn->query($query);
+
+
 
   //Fecha a ligação á base de dados Mysql
-$conn->close();
+  $conn->close();
