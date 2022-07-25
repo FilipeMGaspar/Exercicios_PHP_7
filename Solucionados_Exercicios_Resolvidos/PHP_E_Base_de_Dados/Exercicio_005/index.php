b17c0178afc8ed98ea91db7a1171abee3a2fbdbe
fipewmc <fipewmc2@gmail.com>
2022-07-25 12:20:57 +0100

    PHP e Base de Dados - Exercício 005

---
+++ Solucionados_Exercicios_Resolvidos/PHP_E_Base_de_Dados/Exercicio_005/index.php
@@ -0,0 +1,6 @@
+<?php
+    /*
+        Utilizando a base de dados praticaphp.
+	    Selecione todos os dados da tabela fornecedores utilizando o mysqli, sem utilizar o prepare e bind_param.
+
+    */
