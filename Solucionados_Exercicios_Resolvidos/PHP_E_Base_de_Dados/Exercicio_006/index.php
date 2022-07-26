1d58ce370248fd3cd1cdb63f9fd9bf95330bcf12
fipewmc <fipewmc2@gmail.com>
2022-07-26 14:25:32 +0100

    PHP e Base de Dados - Exercício 006

---
+++ Solucionados_Exercicios_Resolvidos/PHP_E_Base_de_Dados/Exercicio_006/index.php
@@ -0,0 +1,6 @@
+<?php
+/*
+	Selecione os seguintes dados, nome e email da tabela fornecedores onde o id seja maior que 1 utilize o mysqli, deve utilizar o prepare e bind_param.
+
+	SELECT nome, email FROM fornecedores where id > 1;
+*/ 
