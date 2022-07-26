3efe699b24fd853765df841323e9515904f5f68b
fipewmc <fipewmc2@gmail.com>
2022-07-26 14:28:10 +0100

    PHP e Base de Dados - Exercício 006

--- Solucionados_Exercicios_Resolvidos/PHP_E_Base_de_Dados/Exercicio_006/index.php
+++ Solucionados_Exercicios_Resolvidos/PHP_E_Base_de_Dados/Exercicio_006/index.php
@@ -1,6 +1,7 @@
 <?php
 /*
-	Selecione os seguintes dados, nome e email da tabela fornecedores onde o id seja maior que 1 utilize o mysqli, deve utilizar o prepare e bind_param.
+    Utilizando a base de dados ..: praticaphp.
+	Selecione os seguintes dados, nome e email da tabela ..: fornecedores :.. onde o id seja maior que 1 utilize o mysqli, deve utilizar o prepare e bind_param.
 
 	SELECT nome, email FROM fornecedores where id > 1;
 */ 
