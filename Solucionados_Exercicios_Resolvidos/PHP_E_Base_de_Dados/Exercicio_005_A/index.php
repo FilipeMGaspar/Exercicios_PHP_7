13b53b24bfb0e95166dc82f5ab7e1a0e5ee362dd
fipewmc <fipewmc2@gmail.com>
2022-07-26 14:07:43 +0100

    PHP e Base de Dados - Exercício 005 A

--- Solucionados_Exercicios_Resolvidos/PHP_E_Base_de_Dados/Exercicio_005_A/index.php
+++ Solucionados_Exercicios_Resolvidos/PHP_E_Base_de_Dados/Exercicio_005_A/index.php
@@ -3,3 +3,9 @@
         Utilizando a base de dados ..: contas
 	    Selecione todos os dados da tabela ..: cobradores :.. utilizando o mysqli, sem utilizar o prepare e bind_param.
     */
+
+    //Ligação á base de dados
+    $host = "Localhost";
+    $user = "filipe";
+    $pass = "teste123";
+    $db = "contas";
