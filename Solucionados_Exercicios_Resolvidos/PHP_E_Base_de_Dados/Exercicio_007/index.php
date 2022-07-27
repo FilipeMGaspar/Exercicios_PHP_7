70ea8fb23b51ab3a93b0aaa70be92c55248d2785
fipewmc <fipewmc2@gmail.com>
2022-07-27 11:17:33 +0100

    PHP e Base de Dados - Exercício 007

---
+++ Solucionados_Exercicios_Resolvidos/PHP_E_Base_de_Dados/Exercicio_007/index.php
@@ -0,0 +1,7 @@
+<?php
+/*
+    Utilizando a base de dados ..: praticaphp
+    Selecione os seguintes dados, nome e email da tabela ..: fornecedores :.. onde o codigo seja maior que 2 utilize o mysqli, deve utilizar o prepare e bind_param. Coloque os dados numa variável. Mostre os dados contidos na variável no monitor. 
+
+    SELECT nome, email FROM fornecedores where id > 2;
+*/
