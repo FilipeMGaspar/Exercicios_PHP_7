ba1595ac70217a2584c21316ec28bfc67695a9db
fipewmc <fipewmc2@gmail.com>
2022-07-27 10:52:19 +0100

    PHP e Base de Dados - Exercício 006 A

---
+++ Solucionados_Exercicios_Resolvidos/PHP_E_Base_de_Dados/Exercicio_006_A/index.php
@@ -0,0 +1,7 @@
+<?php
+    /*
+    Utilizando a base de dados ..: contas
+	Selecione os seguintes dados, nome, servico e custo da tabela ..: cobradores :.. onde o codigo seja maior que 3 utilize o mysqli, deve utilizar o prepare e bind_param.
+
+	SELECT nome, email FROM fornecedores where id > 3;
+*/ 
