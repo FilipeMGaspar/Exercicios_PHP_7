b2e9779cae7d828ca4fbabdbe0249ae648423902
fipewmc <fipewmc2@gmail.com>
2022-07-29 15:27:32 +0100

    PHP e Base de Dados - Exercício 007

--- Solucionados_Exercicios_Resolvidos/PHP_E_Base_de_Dados/Exercicio_007/index.php
+++ Solucionados_Exercicios_Resolvidos/PHP_E_Base_de_Dados/Exercicio_007/index.php
@@ -3,7 +3,6 @@
     Utilizando a base de dados ..: praticaphp
     Selecione os seguintes dados, nome e email da tabela ..: fornecedores :.. onde o codigo seja maior que 2 utilize o mysqli, deve utilizar o prepare e bind_param. Coloque os dados numa variável. Mostre os dados contidos na variável no monitor. 
 
-    SELECT nome, email FROM fornecedores where codigo > 2;
 */
 
     $host = "localhost";
@@ -18,3 +17,7 @@
     $querySql = "SELECT nome, email FROM fornecedores WHERE codigo > ?";
 
     $stmt = $conn->prepare($querySql);
+
+    $stmt->bind_param("i", $id);
+
+    $stmt->execute();
