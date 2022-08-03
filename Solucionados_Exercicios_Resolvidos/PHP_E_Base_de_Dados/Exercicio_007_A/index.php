c69c8d925696325c7ff1ba275c10a52b1d4f3de0
fipewmc <fipewmc2@gmail.com>
2022-08-03 10:56:48 +0100

    PHP e Base de Dados - Exercício 007 A

--- Solucionados_Exercicios_Resolvidos/PHP_E_Base_de_Dados/Exercicio_007_A/index.php
+++ Solucionados_Exercicios_Resolvidos/PHP_E_Base_de_Dados/Exercicio_007_A/index.php
@@ -1,6 +1,6 @@
 <?php
 /*
-    Utilizando a base de dados ..: contas
+    Utilizando a base de dados ..: praticaphp
     Selecione os seguintes dados:  nome, servico e custo da tabela ..: cobradores :.. onde o idconta seja maior que 2 utilize o mysqli, deve utilizar o prepare e bind_param. Coloque os dados numa variável. Mostre os dados contidos na variável no monitor. 
 
 */
