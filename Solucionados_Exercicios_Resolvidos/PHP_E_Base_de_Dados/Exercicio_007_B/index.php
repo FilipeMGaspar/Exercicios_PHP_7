04872864a4316722bb4ff9ccea02ab02ab4502d7
fipewmc <fipewmc2@gmail.com>
2022-08-03 11:22:22 +0100

    PHP e Base de Dados - Exercício 007 B

--- Solucionados_Exercicios_Resolvidos/PHP_E_Base_de_Dados/Exercicio_007_B/index.php
+++ Solucionados_Exercicios_Resolvidos/PHP_E_Base_de_Dados/Exercicio_007_B/index.php
@@ -1,5 +1,31 @@
 <?php
     /*
-         Utilizando a base de dados ..: praticaphp
-         
+        Utilizando a base de dados ..: praticaphp
+        Crie a tabela ..: people
+
+        idPeople    Inteiro             Chave primária auto incremento
+        username    caracteres(15)      Não pode ser nulo
+        gender      carcater(1)         Não pode ser nulo
+        country     caracter(10)        Não pode ser nulo
+
+
+        CREATE TABLE people (
+        	idPeople INT AUTO_INCREMENT PRIMARY KEY,
+            username VARCHAR(15) NOT NULL,
+            gender CHARACTER NOT NULL,
+            country VARCHAR(10) NOT NULL
+        );
+
+        dados a inserir na tabela
+
+        INSERT INTO people (username, gender, country) VALUES (
+    Mike, m, USA,
+    SARAH, f, Denmark,
+    Gregory, m, UK,
+    Julie, f, USA,
+    Maria, f, Mexico,
+    Henry, m, China.
+    Beth, f, Canada,
+    Sandeep, m, india
+);
     */ 
