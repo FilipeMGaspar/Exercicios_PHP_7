95bc79e508dbd0943be55f712758eb05696d0e4f
fipewmc <fipewmc2@gmail.com>
2022-08-05 08:26:58 +0100

    PHP e Base de Dados - Exercício 007 E

--- Solucionados_Exercicios_Resolvidos/PHP_E_Base_de_Dados/Exercicio_007_E/index.php
+++ Solucionados_Exercicios_Resolvidos/PHP_E_Base_de_Dados/Exercicio_007_E/index.php
@@ -1,6 +1,6 @@
 <?php
     /*
         Utilizando a base de dados ..: praticaphp
-        Selecione os seguintes registos, username, gender e country da tabela ..: people
-        Mostre os dados no Monitor
+        Selecione os seguintes registos, username, gender e country  da tabela ..: people onde o idPeople seja maior que 2 utilize o mysqli, deve utilizar o prepare e bind_param.
+        Mostre os dados.
     */ 
