26e70287b7d10dc07f460e2bdcc4a2a1de36714f
fipewmc <fipewmc2@gmail.com>
2022-08-05 08:22:39 +0100

    PHP e Base de Dados - Exercício 007 D

--- Solucionados_Exercicios_Resolvidos/PHP_E_Base_de_Dados/Exercicio_007_D/index.php
+++ Solucionados_Exercicios_Resolvidos/PHP_E_Base_de_Dados/Exercicio_007_D/index.php
@@ -5,23 +5,23 @@
     Mostre os dados no Monitor
 */ 
 
-//Ligação á base de dados
-$host = "localhost";
-$user = "root";
-$pass = "";
-$db = "praticaphp";
+    //Ligação á base de dados
+    $host = "localhost";
+    $user = "root";
+    $pass = "";
+    $db = "praticaphp";
 
-$conn = new mysqli($host, $user, $pass, $db);
+    $conn = new mysqli($host, $user, $pass, $db);
 
-$querySQL = "SELECT * FROM people";
+    $querySQL = "SELECT * FROM people";
 
-$stmt = $conn->prepare($querySQL);
+    $stmt = $conn->prepare($querySQL);
 
-$stmt->execute();
+    $stmt->execute();
 
-$dados = $stmt->get_result();
-echo "<table>";
-echo "<tr> <td># ID</td> <td>Username</td> <td>Gender</td> <td>Country</td> </tr>";
-while ($dado = $dados->fetch_assoc()) {
-    echo "<tr> <td>" . $dado["idPeople"] . "</td><td>". $dado["username"] . "</td><td>" . $dado["gender"] . "</td><td>" . $dado["country"] ."</td></tr>";
-}
+    $dados = $stmt->get_result();
+    echo "<table>";
+    echo "<tr> <td># ID</td> <td>Username</td> <td>Gender</td> <td>Country</td> </tr>";
+    while ($dado = $dados->fetch_assoc()) {
+        echo "<tr> <td>" . $dado["idPeople"] . "</td><td>". $dado["username"] . "</td><td>" . $dado["gender"] . "</td><td>" . $dado["country"] ."</td></tr>";
+    }
