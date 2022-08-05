b01ed352a44f7def0e6046886c1418cb9bc8f8ae
fipewmc <fipewmc2@gmail.com>
2022-08-05 08:25:53 +0100

    PHP e Base de Dados - Exercício 007 E

---
+++ Solucionados_Exercicios_Resolvidos/PHP_E_Base_de_Dados/Exercicio_007_E/index.php
@@ -0,0 +1,6 @@
+<?php
+    /*
+        Utilizando a base de dados ..: praticaphp
+        Selecione os seguintes registos, username, gender e country da tabela ..: people
+        Mostre os dados no Monitor
+    */ 
