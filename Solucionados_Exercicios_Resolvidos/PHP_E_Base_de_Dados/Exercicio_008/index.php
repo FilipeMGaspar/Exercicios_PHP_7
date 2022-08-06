061d5b4f25ef818dbb11818991bc298b5e2a291b
fipewmc <fipewmc2@gmail.com>
2022-08-06 16:17:06 +0100

    PHP e Base de Dados - Exercício 002 A

--- Solucionados_Exercicios_Resolvidos/PHP_E_Base_de_Dados/Exercicio_008/index.php
+++ Solucionados_Exercicios_Resolvidos/PHP_E_Base_de_Dados/Exercicio_008/index.php
@@ -1,7 +1,7 @@
 <?php
  /*
     Utilizando a base de dados ..: praticaphp.
-	Altere os seguintes dados, nome da tabela ..: fornecedores :.. onde o id seja igual que 1 utilize o mysqli, deve utilizar o prepare e bind_param. Mostre o resultado no monitor.
+	Altere o registo nome para *** Ricardo Arrigoni ** na tabela ..: fornecedores :.. onde o codigo seja igual que 1 utilize o mysqli, deve utilizar o prepare e bind_param. Mostre o resultado no monitor.
 
     UPDATE fornecedores SET nome=”Ricardo Arrigoni” WHERE codigo=1;
  */ 
@@ -13,3 +13,8 @@
     $db = "praticaphp";
 
     $conn = new mysqli($host, $user, $pass, $db);
+
+    //Definição do id
+    $id = 1;
+
+    $querySql = "UPDATE fornecedores SET nome='Ricardo Arrigoni' WHERE codigo = ?";
