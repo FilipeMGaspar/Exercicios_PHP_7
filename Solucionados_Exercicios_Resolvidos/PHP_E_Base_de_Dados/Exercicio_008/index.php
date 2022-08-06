2a489b2525bda81e922419d29e2aad114d8eca4e
fipewmc <fipewmc2@gmail.com>
2022-08-06 16:32:44 +0100

    PHP e Base de Dados - Exercício 008

--- Solucionados_Exercicios_Resolvidos/PHP_E_Base_de_Dados/Exercicio_008/index.php
+++ Solucionados_Exercicios_Resolvidos/PHP_E_Base_de_Dados/Exercicio_008/index.php
@@ -3,7 +3,6 @@
     Utilizando a base de dados ..: praticaphp.
 	Altere o registo nome para *** Ricardo Arrigoni ** na tabela ..: fornecedores :.. onde o codigo seja igual que 1 utilize o mysqli, deve utilizar o prepare e bind_param. Mostre o resultado no monitor.
 
-    UPDATE fornecedores SET nome=”Ricardo Arrigoni” WHERE codigo=1;
  */ 
 
     //Ligação á base de dados
