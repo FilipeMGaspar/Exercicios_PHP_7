cb18745431231ff5f016df016787c0c543901c89
fipewmc <fipewmc2@gmail.com>
2022-08-06 16:45:43 +0100

    PHP e Base de Dados - Exercício 008 A

--- Solucionados_Exercicios_Resolvidos/PHP_E_Base_de_Dados/Exercicio_008_A/index.php
+++ Solucionados_Exercicios_Resolvidos/PHP_E_Base_de_Dados/Exercicio_008_A/index.php
@@ -2,5 +2,10 @@
 /*
     Utilizando a base de dados ..: contas
     Altere os seguintes dados ** custo ** para 32.65 na tabela ..: cobradores :.. onde o idconta seja igual a 4 utilize o mysqli, deve utilizar o prepare e bind_param. Mostre o resultado no monitor.
-
 */ 
+
+//Ligação á base de dados
+$host = "localhost";
+$user = "root";
+$pass = "";
+$db = "contas";
