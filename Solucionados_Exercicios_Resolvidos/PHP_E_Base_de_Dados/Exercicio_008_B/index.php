e275dae65329cdd04225fa803c130fc439512f55
fipewmc <fipewmc2@gmail.com>
2022-08-09 12:01:46 +0100

    PHP e Base de Dados - Exercício 008 B

---
+++ Solucionados_Exercicios_Resolvidos/PHP_E_Base_de_Dados/Exercicio_008_B/index.php
@@ -0,0 +1,6 @@
+<?php
+    /*
+        Utilizando a base de dados ..: praticaphp
+        Altere os seguintes registos: username de ** Henry ** para * HenryP  na tabela ..: people onde o username seja igual a Henry utilize o mysqli, deve utilizar o prepare e bind_param.
+        Mostre os dados.
+    */ 
