875cc5accc83c840b9dd6a8a310c9ef27068f758
fipewmc <fipewmc2@gmail.com>
2022-08-09 12:37:08 +0100

    PHP e Base de Dados - Exercício 009

--- Solucionados_Exercicios_Resolvidos/PHP_E_Base_de_Dados/Exercicio_009/index.php
+++ Solucionados_Exercicios_Resolvidos/PHP_E_Base_de_Dados/Exercicio_009/index.php
@@ -1,10 +1,7 @@
 <?php
 /*
-    Utilizando a base de dados praticaphp.
-	Apague os seguintes dados da tabela fornecedores onde o id seja igual que 3 utilize o mysqli, deve utilizar o prepare e bind_param.
+     Utilizando a base de dados ..: praticaphp
+	Apague os seguintes dados dna tabela ..: fornecedores :.. onde o codigo seja igual que 3 utilize o mysqli, deve utilizar o prepare e bind_param.
 
 	DELETE FROM fornecedores WHERE codigo=3;
-
-     Utilizando a base de dados ..: praticaphp.
-	Altere o registo nome para *** Ricardo Arrigoni ** na tabela ..: fornecedores :.. onde o codigo
 */ 
