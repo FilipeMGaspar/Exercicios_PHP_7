80064e3dc190ace6e9ce5a0c97951274a856b6b1
fipewmc <fipewmc2@gmail.com>
2022-08-11 13:35:31 +0100

    PHP e Base de Dados - Exercício 009

--- Solucionados_Exercicios_Resolvidos/PHP_E_Base_de_Dados/Exercicio_009/index.php
+++ Solucionados_Exercicios_Resolvidos/PHP_E_Base_de_Dados/Exercicio_009/index.php
@@ -3,7 +3,6 @@
      Utilizando a base de dados ..: praticaphp
 	Apague os seguintes dados dna tabela ..: fornecedores :.. onde o codigo seja igual que 3 utilize o mysqli, deve utilizar o prepare e bind_param.
 
-	DELETE FROM fornecedores WHERE codigo=3;
 */ 
 
 //Ligação á base de dados
@@ -14,3 +13,5 @@ $db = "praticaphp";
 
 $conn = new mysqli($host, $user, $pass, $db);
 
+// Query sql e execução
+$querySql = "DELETE FROM fornecedores WHERE codigo = ?";
