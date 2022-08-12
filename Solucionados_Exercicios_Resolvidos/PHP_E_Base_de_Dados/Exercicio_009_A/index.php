d1f34e2b9fb07fa2e0728b59fede2828dbb260a4
fipewmc <fipewmc2@gmail.com>
2022-08-12 13:34:00 +0100

    PHP e Base de Dados - Exercício 009 A

--- Solucionados_Exercicios_Resolvidos/PHP_E_Base_de_Dados/Exercicio_009_A/index.php
+++ Solucionados_Exercicios_Resolvidos/PHP_E_Base_de_Dados/Exercicio_009_A/index.php
@@ -1,5 +1,14 @@
 <?php
 /*
     Utilizando a base de dados ..: contas
-    Apague os seguintes dados na tabela ..: cobradores :.. onde o idconta seja igual a 3 utilize o mysqli, deve utilizar o prepare e bind_param. Mostre o resultado no monitor.
+    Apague os seguintes dados na tabela ..: cobradores :.. onde o idconta seja igual a 3 utilize o mysqli, deve utilizar o prepare e bind_param. 
+    Mostre o resultado no monitor.
 */ 
+
+//Ligação á base de dados
+    $host = "localhost";
+    $user = "root";
+    $pass = "";
+    $db = "contas";
+
+    
