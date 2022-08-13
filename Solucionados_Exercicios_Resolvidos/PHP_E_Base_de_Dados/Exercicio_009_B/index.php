13cfc47794e9138972d92292819b8ef73ff5e1db
fipewmc <fipewmc2@gmail.com>
2022-08-13 15:53:57 +0100

    PHP e Base de Dados - Exercício 009 B

--- Solucionados_Exercicios_Resolvidos/PHP_E_Base_de_Dados/Exercicio_009_B/index.php
+++ Solucionados_Exercicios_Resolvidos/PHP_E_Base_de_Dados/Exercicio_009_B/index.php
@@ -4,3 +4,9 @@
     Apague os seguintes dados na tabela ..: people :.. onde o idconta seja igual a 4 utilize o mysqli, deve utilizar o prepare e bind_param. 
     Mostre o resultado no monitor.
 */ 
+
+// Ligação á base de dados
+    $host = "localhost";
+    $user = "root";
+    $pass = "";
+    $db = "praticaphp";
