c20a2bf4e4f99a7620885174e37b192c69f07dba
fipewmc <fipewmc2@gmail.com>
2022-08-13 16:11:18 +0100

    PHP e Base de Dados - Exercício 010

---
+++ Solucionados_Exercicios_Resolvidos/PHP_E_Base_de_Dados/Exercicio_010/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-pt">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercicio 010 | PHP e Base de dados</title>
+</head>
+<body>
+    <div class="container">
+        <div class="formulario">
+            <form action="" method="post"></form>
+        </div>
+    </div>
+</body>
+</html>
