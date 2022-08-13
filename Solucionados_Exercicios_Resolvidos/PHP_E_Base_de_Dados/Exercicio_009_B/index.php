6b2dde3a2b5484d122ec29dc2eb3f9c7e56a92b5
fipewmc <fipewmc2@gmail.com>
2022-08-13 16:00:41 +0100

    PHP e Base de Dados - Exercício 009 B

--- Solucionados_Exercicios_Resolvidos/PHP_E_Base_de_Dados/Exercicio_009_B/index.php
+++ Solucionados_Exercicios_Resolvidos/PHP_E_Base_de_Dados/Exercicio_009_B/index.php
@@ -1,7 +1,7 @@
 <?php
 /*
     Utilizando a base de dados ..: praticaphp
-    Apague os seguintes dados na tabela ..: people :.. onde o idconta seja igual a 4 utilize o mysqli, deve utilizar o prepare e bind_param. 
+    Apague os seguintes dados na tabela ..: people :.. onde o idpeople seja igual a 4 utilize o mysqli, deve utilizar o prepare e bind_param. 
     Mostre o resultado no monitor.
 */ 
 
@@ -13,5 +13,17 @@
 
     $conn = new mysqli($host, $user, $pass, $db);
 
+    $idPeople = 3;
+/*
     // Query sql e execução
-    
+    $stmt = $conn->prepare("DELETE FROM people WHERE idpeople = ?");
+    $stmt->bind_param("i", $idPeople);
+    $stmt->execute();
+*/
+    // Mostrar dados no Front
+    echo "<table>";
+    echo "<th><td> <strong> IdPeople </strong> </td> <td> <strong> UserName </strong> </td> <td> <strong> Gender </strong> </td> <td> <strong> Country </strong> </td> </th>";
+    echo "</table>";
+    // Encerramento da ligação á base de dados
+   // $stmt->close();
+    $conn->close();
