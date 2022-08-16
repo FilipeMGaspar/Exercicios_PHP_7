789a9d4b91937c63b81a12b73b5f3c930a2a77d9
fipewmc <fipewmc2@gmail.com>
2022-08-16 12:20:54 +0100

    PHP e Base de Dados - Exercício 010 A

---
+++ Solucionados_Exercicios_Resolvidos/PHP_E_Base_de_Dados/Exercicio_010_A/regista.php
@@ -0,0 +1,11 @@
+<?php
+    /*  Modificado para utilizar formulário em html 5
+
+        Utilizando a base de dados. .: praticaphp.
+	    Insira os seguintes dados na tabela ..: fornecedores :.. utilizando o mysqli, deve utilizar o prepare e bind_param.
+
+        codigo	        4	                     5	                       6
+        nome	      Guilherme	              Cristina	                Filipe
+        email	    guigu@gmail.com	        nita@gmail.com	        filipe@gmail.com			
+
+    */
