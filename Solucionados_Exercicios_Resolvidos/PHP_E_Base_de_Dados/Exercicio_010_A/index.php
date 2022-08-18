c394a01989ad54bfaa7918bd0749bd260591f4bc
fipewmc <fipewmc2@gmail.com>
2022-08-18 19:22:59 +0100

    PHP e Base de Dados - Exercício 010 A

--- Solucionados_Exercicios_Resolvidos/PHP_E_Base_de_Dados/Exercicio_010_A/index.php
+++ Solucionados_Exercicios_Resolvidos/PHP_E_Base_de_Dados/Exercicio_010_A/index.php
@@ -1,3 +1,18 @@
+<?php
+   /*  Modificado para utilizar formulário em html 5
+
+        Utilizando a base de dados. .: contas
+	    Insira os seguintes dados na tabela ..: cobradores :.. utilizando o mysqli, deve utilizar o prepare e bind_param.
+
+        codigo	        5	               6
+        nome	      Galp              Montepio	                
+        servico	    Combustivel	          Banco	     			
+        custo           20                 4.6
+
+    */
+    
+?>
+
 <!DOCTYPE html>
 <html lang="pt-pt">
 <head>
--- Solucionados_Exercicios_Resolvidos/PHP_E_Base_de_Dados/Exercicio_010_A/regista.php
+++ Solucionados_Exercicios_Resolvidos/PHP_E_Base_de_Dados/Exercicio_010_A/regista.php
@@ -1,12 +1,2 @@
 <?php
-    /*  Modificado para utilizar formulário em html 5
-
-        Utilizando a base de dados. .: contas
-	    Insira os seguintes dados na tabela ..: cobradores :.. utilizando o mysqli, deve utilizar o prepare e bind_param.
-
-        codigo	        5	               6
-        nome	      Galp              Montepio	                
-        servico	    Combustivel	          Banco	     			
-        custo           20                 4.6
-
-    */
+ 
