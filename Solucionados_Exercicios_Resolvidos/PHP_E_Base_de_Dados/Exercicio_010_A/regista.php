d2b0150af3914e94cd8059b778bcc53784db3615
fipewmc <fipewmc2@gmail.com>
2022-08-19 09:40:33 +0100

    PHP e Base de Dados - Exercício 010 A

--- Solucionados_Exercicios_Resolvidos/PHP_E_Base_de_Dados/Exercicio_010_A/regista.php
+++ Solucionados_Exercicios_Resolvidos/PHP_E_Base_de_Dados/Exercicio_010_A/regista.php
@@ -5,9 +5,13 @@
     $servico = filter_input(INPUT_POST, "servico");
     $custo = filter_input(INPUT_POST, "custo");
 
-    $stmt = $conn->prepare("INSERT INTO cobradores (nome. servico, custo) VALUES (?, ? ,?)");
+    $custo = number_format($custo, 2, ',', ' '); // Formatção do número com duas casas decimais
+
+    $stmt = $conn->prepare("INSERT INTO cobradores (nome, servico, custo) VALUES (?, ? ,?)");
     $stmt->bind_param("ssd", $nome, $servico, $custo);
     $stmt->execute();
 
     $stmt->close();
     $conn->close();
+
+    header("Location: " . $_SERVER["HTTP_REFERER"]); // Redireciona a página
