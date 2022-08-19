90711583948dfa779d2d9fc7652973a5e919aa0d
fipewmc <fipewmc2@gmail.com>
2022-08-19 09:33:21 +0100

    PHP e Base de Dados - Exercício 010 A

--- Solucionados_Exercicios_Resolvidos/PHP_E_Base_de_Dados/Exercicio_010_A/regista.php
+++ Solucionados_Exercicios_Resolvidos/PHP_E_Base_de_Dados/Exercicio_010_A/regista.php
@@ -3,4 +3,6 @@
 
     $nome = filter_input(INPUT_POST, "nome");
     $servico = filter_input(INPUT_POST, "servico");
-    
+    $custo = filter_input(INPUT_POST, "custo");
+
+    echo "$nome | $servico | $custo";
