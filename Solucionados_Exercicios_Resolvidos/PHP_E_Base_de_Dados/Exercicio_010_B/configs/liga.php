6185971ff08fbe86e84cccfe7953aa40ff2a32eb
fipewmc <fipewmc2@gmail.com>
2022-08-20 12:33:31 +0100

    PHP e Base de Dados - Exercício 010 B

---
+++ Solucionados_Exercicios_Resolvidos/PHP_E_Base_de_Dados/Exercicio_010_B/configs/liga.php
@@ -0,0 +1,16 @@
+<?php
+/*
+        Utilizando a base de dados ..: praticaphp
+        Insira os dados abaixo na tabela ..: people
+
+        idPeople        username        gender          country
+
+            1             Mike            m               USA
+            2             Sarah           f               Denmark
+            3             Gregory         m               UK
+            4             Julie           f               USA
+            5             Maria           f               Mexico
+            6             Henry           m               China
+            7             Beth            f               Canada
+            8             Sandeep         m               India
+*/
