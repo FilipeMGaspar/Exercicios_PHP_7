2d119383e4f09d7097ecea0d4d534237f36d2691
fipewmc <fipewmc2@gmail.com>
2022-08-22 10:03:55 +0100

    PHP e Base de Dados - Exercício 010 B

--- Solucionados_Exercicios_Resolvidos/PHP_E_Base_de_Dados/Exercicio_010_B/regista.php
+++ Solucionados_Exercicios_Resolvidos/PHP_E_Base_de_Dados/Exercicio_010_B/regista.php
@@ -7,7 +7,12 @@
     $gender = filter_input(INPUT_POST, "gender");
     $country = filter_input(INPUT_POST, "country");
 
-    
+    $stmt = $conn->prepare("INSERT INTO people (username, gender, country) VALUES (?, ?, ?)");
+    $stmt->bind_param("sss",  $username, $gender, $country);
+    $stmt->execute();
+
+    $stmt->close();
+    $conn->close();
 
     $_SESSION["msg"] = "Registo efetuado com sucesso!";
     $_SESSION["tipoOp"] = "sucesso";
