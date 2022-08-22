c033d782bdc4e9861521ee0f81c089255484b2d4
fipewmc <fipewmc2@gmail.com>
2022-08-22 09:20:14 +0100

    PHP e Base de Dados - Exercício 010 B

--- Solucionados_Exercicios_Resolvidos/PHP_E_Base_de_Dados/Exercicio_010_B/regista.php
+++ Solucionados_Exercicios_Resolvidos/PHP_E_Base_de_Dados/Exercicio_010_B/regista.php
@@ -1,6 +1,8 @@
 <?php
     require_once "configs/liga.php";
+    require_once "configs/msgs.php";
 
     $data = $_POST;
 
-    print_r($data);
+    //print_r($data);
+    $msgTxt = new Mensagens();
