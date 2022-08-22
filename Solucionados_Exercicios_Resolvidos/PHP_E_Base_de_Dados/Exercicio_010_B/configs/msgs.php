bcd483f66da2112d3c36b1e447a190871b2e615b
fipewmc <fipewmc2@gmail.com>
2022-08-22 09:18:56 +0100

    PHP e Base de Dados - Exercício 010 B

--- Solucionados_Exercicios_Resolvidos/PHP_E_Base_de_Dados/Exercicio_010_B/configs/msgs.php
+++ Solucionados_Exercicios_Resolvidos/PHP_E_Base_de_Dados/Exercicio_010_B/configs/msgs.php
@@ -4,6 +4,12 @@
     private $msg;
     private $type;
 
-    
+    public function setMsg($mensagem) {
+      $this->msg = $mensagem;
+    }
+
+    public function getMsg() {
+      return $this->msg;
+    }
 
  }
