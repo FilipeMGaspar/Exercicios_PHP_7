5ad7e7257b9643743a212d4a6bb79beeb3ad3f97
fipewmc <fipewmc2@gmail.com>
2022-08-22 09:59:49 +0100

    PHP e Base de Dados - Exercício 010 B

--- Solucionados_Exercicios_Resolvidos/PHP_E_Base_de_Dados/Exercicio_010_B/regista.php
+++ Solucionados_Exercicios_Resolvidos/PHP_E_Base_de_Dados/Exercicio_010_B/regista.php
@@ -4,7 +4,24 @@
     require_once "configs/liga.php";
 
     $data = $_POST;
+    $username = filter_input(INPUT_POST, "username");
+    $gender = filter_input(INPUT_POST, "gender");
+    $country = filter_input(INPUT_POST, "country");
 
-    $_SESSION["msg"] = "Registo efetuado com sucesso!";
+
+    $_SESSION["msg"] = $gender;
+    //$_SESSION["msg"] = "Registo efetuado com sucesso!";
     $_SESSION["tipoOp"] = "sucesso";
+
     header("Location: " . $_SERVER["HTTP_REFERER"]); // Redireciona a página
+
+    /*
+        Utilizando a base de dados ..: praticaphp
+        Insira os dados abaixo na tabela ..: people
+
+        idPeople        username        gender          country
+
+            9             Josefina        f               França
+            10            Mário           m               Portugal   
+            11            Luisa           f               Espanha
+*/
