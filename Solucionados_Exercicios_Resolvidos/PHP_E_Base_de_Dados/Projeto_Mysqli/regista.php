c18b164ecd5f53b6b0d5ea8d04d807864376fc1d
fipewmc <fipewmc2@gmail.com>
2022-08-28 11:45:28 +0100

    Projeto PHP 7 com Mysqli - PHP e Base de Dados

--- Solucionados_Exercicios_Resolvidos/PHP_E_Base_de_Dados/Projeto_Mysqli/regista.php
+++ Solucionados_Exercicios_Resolvidos/PHP_E_Base_de_Dados/Projeto_Mysqli/regista.php
@@ -1,7 +1,11 @@
 <?php
 
+    $formType = filter_input(INPUT_POST, "type");
     $nome = filter_input(INPUT_POST, "name");
     $sobrenome = filter_input(INPUT_POST, "lastname");
     $email = filter_input(INPUT_POST, "email");
     $pass = filter_input(INPUT_POST, "password");
     $confirmPass = filter_input(INPUT_POST, "confirmpass");
+
+    echo $formType;
+    echo "<br>";
