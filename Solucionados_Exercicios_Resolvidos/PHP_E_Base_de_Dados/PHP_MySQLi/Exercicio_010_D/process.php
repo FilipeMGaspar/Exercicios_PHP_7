da608a2fc9f75e65a53bcfd7350d5ebf3c9ae9bd
fipewmc <fipewmc2@gmail.com>
2022-09-08 00:06:04 +0100

    PHP e Base de Dados - Exercício 010 D

--- Solucionados_Exercicios_Resolvidos/PHP_E_Base_de_Dados/PHP_MySQLi/Exercicio_010_D/process.php
+++ Solucionados_Exercicios_Resolvidos/PHP_E_Base_de_Dados/PHP_MySQLi/Exercicio_010_D/process.php
@@ -1,5 +1,6 @@
 <?php
     require_once "configs/liga.php";
+    require_once "configs/funcoes.php";
 
     $type = filter_input(INPUT_POST, "type");
     $email = filter_input(INPUT_POST, "email");
@@ -11,7 +12,7 @@
         if($type === "login") {
 
            if (findByEmail($email, $conn)) {
-                //verifica a password recebida com a password guardada na base de dados
+                
                 $stmt = $conn->prepare("SELECT email, password FROM utilizadores WHERE  email = ? LIMIT 1");
                 $stmt->bind_param("s", $email);
                 
@@ -23,6 +24,7 @@
                     $error = $e->getMessage();
                 } 
 
+                //verifica a password recebida com a password guardada na base de dados
                 if (testaPassHash($pass, $resultado["password"])) {
                     echo "<br>Seja bem vindo!";
                 } else {
@@ -67,50 +69,3 @@
         // $_SESSION["msg"] = "Formulário desconhecido!";
         header("Location: " . $_SERVER["HTTP_REFERER"]); // Redireciona a página
     }
-
-    // Funções para o funcionamento do programa 
-      
-    // Função para verificar se já existe um email igual no base de dados
-    function findByEmail($mail, mysqli $conet) {
-
-        $stmt = $conet->prepare("SELECT email FROM utilizadores WHERE email = ? LIMIT 1");
-        $stmt->bind_param("s", $mail);
-
-        try {           
-            $stmt->execute();           
-        } catch (Exception $e){
-            $error = $e->getMessage();
-        }
-        $dados = $stmt->get_result();
-
-        if($dados->num_rows > 0) {
-            return true;
-        } else {
-            return false;
-        }
-
-    }
-
-
-    // Função para encriptar a palavra passe
-    function criptoPass($passwd) { 
-        $secPass = md5($passwd);
-        $secPass = base64_encode($secPass);
-
-        return $secPass;
-    }
-
-    // Função para gerar uma hash da palavra passe para  que esta seja guardada na base de dados
-    function gerarPasswdHash($palavraPass) { 
-        $txt = criptoPass($palavraPass);
-        $hash = password_hash($txt, PASSWORD_DEFAULT);
-
-        return $hash;
-    }
-
-    // Funçaõ que com para a password recebida do formulário com a password guardada na base de daddos
-    function testaPassHash($palavraPass, $passwdHash) { 
-        $testaPass = password_verify(criptoPass($palavraPass), $passwdHash);
-
-        return $testaPass;
-    }   
